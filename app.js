be66c66323283b6319211c47c08751c4eac45acb
ismail <ismailjosim99@gmail.com>
2023-08-16 22:41:48 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const listEl = document.getElementById('myList');
+const btnEl = document.getElementById('add-btn');
+
+// HTMLCollection - live collection
+const htmlCollection = ul.getElementsByTagName('li');
+btnEl.addEventListener('click', function () {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Orange';
+    ul.appendChild(newItem);
+    console.log("Total htmlCollection" + " " + htmlCollection.length);  // Updated automatically
+});
+
+// // NodeList - non-live collection
+// const nodeList = ul.querySelectorAll('li');
+// button.addEventListener('click', function () {
+//     const newItem = document.createElement('li');
+//     newItem.textContent = 'Grapes';
+//     ul.appendChild(newItem);
+//     console.log("Total NodeList" + " " + nodeList.length);  // Not automatically updated
+// });
+
